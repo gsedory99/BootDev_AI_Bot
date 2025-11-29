88d1a0235c688c2ee75f810ec66d8460660631f9
Grayson Sedory <gsedory99@gmail.com>
2025-11-28 19:05:13 -0500

    updates

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,5 @@
 import os
+import config
 
 def get_file_content(working_directory, file_path):
     raw_path = os.path.join(working_directory, file_path)
@@ -6,6 +7,11 @@ def get_file_content(working_directory, file_path):
     absolute_working_dir = os.path.abspath(working_directory)
 
     if not os.path.isfile(absolute_file_path):
-        return f'Error: File not found or is not a regular file: "{absolute_file_path}"'
+        return f'Error: File not found or is not a regular file: "{file_path}"'
     if not absolute_file_path.startswith(absolute_working_dir):
-        f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'kk
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    with open(file_path, "r") as f:
+        file_content_string = f.read(config.MAX_CHARS)
+
+    return file_content_string
