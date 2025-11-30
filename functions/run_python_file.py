7ad7e8da0385f60e65a528dcf2d772fc3f6e2b83
Grayson Sedory <gsedory99@gmail.com>
2025-11-29 23:11:56 -0500

    added run python file option

---
+++ functions/run_python_file.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+from typing import final
+
+def run_python_file(working_directory, file_path, args=[]):
+    raw_path = os.path.join(working_directory, file_path)
+    absolute_file_path = os.path.abspath(raw_path)
+    absolute_working_dir = os.path.abspath(working_directory)
+
+    if not absolute_file_path.startswith(absolute_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(absolute_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if absolute_file_path[-3:] != ".py":
+        return f'Error: "{file_path}" is not a Python file.'
+
+    try:
+        result = subprocess.run(
+            args, 
+            timeout=30,
+            cwd=absolute_working_dir,
+            capture_output=True,
+            text=True
+        )
+    except subprocess.TimeoutExpired:
+        return "Error: Process timed out after 30 seconds."
+    
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+
+    final_output = f'STDOUT: {result.stdout}\nSTDERR:{result.stderr}'
+
+    if result.returncode != 0:
+        final_output += f'\nProcess exited with code {result.returncode}'
+
+    if not result.stdout and not result.stderr:
+        return "No output produced."
+
+    return final_output
+    
